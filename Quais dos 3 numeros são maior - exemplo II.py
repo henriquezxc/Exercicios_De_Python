7206a9911fa1c43938c7d32b9d08759965c778ba
Henriquezxc <126987668+henriquezxc@users.noreply.github.com>
2023-03-18 15:09:00 -0300

    Exercicio onde mostra qual maior numero em 3 numeros digitados.

---
+++ Quais dos 3 numeros são maior - exemplo II.py
@@ -0,0 +1,12 @@
+nun1=int(input("DIgite o primeiro numero: "))
+nun2=int(input("Digite o segundn numero: "))
+nun3=int(input("Digite o terçeiro numero: "))
+
+if nun1>nun2 and nun1>nun3:
+    print("O numero: ",nun1,"e o maior de todos")
+elif nun2>nun1 and nun2>nun3:
+    print("O numero: ",nun2, "e o maior de todos")
+elif nun3>nun1 and nun3>nun2:
+    print("O numero: ",nun3,"e o maior de todos")
+elif nun1 == nun2 and nun3:
+    print("são todos iguais, não temos resultado")
