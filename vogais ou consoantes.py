d62b497fce9ea5e0334e298c806e2e2d528fcbdc
henriquezxc <henriqueverisimop@gmail.com>
2023-03-19 00:50:40 -0300

    Pronto.

---
+++ vogais ou consoantes.py
@@ -0,0 +1,36 @@
+# Programa que verifica se a letra e vogal ou consoante:
+
+letra = (input("Digite uma letra: "))
+
+if(letra == "a"):
+    print("Vogal")
+# Criei desta forma com varios "elif" porque e a conheço no momento "Gambiarra"
+elif(letra == "e"):
+    print("vogal")
+elif(letra == "i"):
+    print("vogal")
+elif(letra == "o"):
+    print("vogal")
+elif(letra == "u"):
+    print("vogal")
+elif(letra == "y"):
+    print("vogal")
+elif(letra == "w"):
+    print("vogal")
+elif(letra == "A"):
+    print("vogal")
+elif(letra == "E"):
+    print("vogal")
+elif(letra == "I"):
+    print("vogal")
+elif(letra == "O"):
+    print("vogal")
+elif(letra == "U"):
+    print("vogal")
+elif(letra == "Y"):
+    print("vogal")
+elif(letra == "W"):
+    print("vogal")
+else:
+    print("Consoante")
+# Vogais: a e i o u y wA
