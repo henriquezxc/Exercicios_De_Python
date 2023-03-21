98745570ec88d23813da8c01bcc772fd49df578f
henriquezxc <henriqueverisimop@gmail.com>
2023-03-20 23:43:53 -0300

    Aula nova dia 20/03/23

---
+++ Salario bruto com calculo de, INSS,IR e SINDICATO.py
@@ -0,0 +1,14 @@
+# Valor que custa a hora do trabalhador.
+valor_horas = float(input('Digite Quando você ganha por horas trabalhadas: '))
+# valor que ele ganha por horas:
+horas_trabalhadas = float(input('Digite quantas horas você trabalhou: '))
+# Variavel que soma horas trabalhadas com o valor da hora.
+salario = valor_horas*horas_trabalhadas
+# Variavel que calcula o salario liquido usado no ultimo print.
+liquido= salario*76/100
+# Print com todos os resultados.
+print('Seu salario bruto R$:',salario)
+print('Imposto de renda R$:',salario*11/100)
+print('INSS R$:',salario*8/100)
+print('Sindicato R$:',salario*5/100)
+print('Salario liquido R$:',liquido)
