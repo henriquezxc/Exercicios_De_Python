53f4ea29d1311cbee19e369a72fdcc0be8b98298
henriquezxc <henriqueverisimop@gmail.com>
2023-03-30 20:49:56 -0300

    Acrescentei comentatios ao codigo

--- Exercicios Prontos/Ler a quantidade de digitos de numeros inteiros inseridos.py
+++ Exercicios Prontos/Ler a quantidade de digitos de numeros inteiros inseridos.py
@@ -1,3 +1,5 @@
-texto = (input('Digite uma sequencia de numeros: '))
-quantidade= len (texto)
-print(quantidade)
+# Programa que ler a quantidade de letras inseridas pelo usuario.
+Texto = (input('Digite uma sequencia de numeros: '))
+# Usamos o "LEN " para ler a quantidade.
+Quantidade= len (Texto)
+print(Quantidade)
