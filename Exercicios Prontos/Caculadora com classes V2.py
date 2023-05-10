d07f9c2ac48d121adeb002c0fa477331880e670d
henriquezxc <henriqueverisimop@gmail.com>
2023-05-09 21:22:29 -0300

    Mudanças no codigo

--- Exercicios Prontos/Caculadora com classes V2.py
+++ Exercicios Prontos/Caculadora com classes V2.py
@@ -1,4 +1,6 @@
 from time import sleep
+import os
+
 
 class Calcular():
     def __init__(self,a,b):
@@ -12,30 +14,47 @@ class Calcular():
         return self.numero1 - self.numero2
     def Dividir(self):
         return self.numero1 / self.numero2
-try:
-    Entrada_1 = int(input('Digite o primeiro numero: '))
-    Entrada_2 = int(input('Digite o segundo numero: '))
 
-    Resultado = Calcular(Entrada_1,Entrada_2)
+def App():
+    Entrada_Temp = (input('Deseja fazer outro cálculo? (SIM) ou (NÃO):').upper())
+    if Entrada_Temp == 'SIM':
+        os.system("clear")
+        Calculos()
+        
+    else:
+        print('Saindo do programa...')
+        sleep(2)
 
-    print('Estamos fazendo calculo, aguarde...')
-    sleep(2)
-    print('So mais um momento...')
-    sleep(2)
 
-    print(f'''
-    Resultado:
-    A Soma é de: {Resultado.somar()}
-    A Subtração é de: {Resultado.Subtrair()}
-    A Multiplicação é de: {Resultado.multiplicar()}
-    A Divisão é de: {Resultado.Dividir():.2f}
-    ''')
+def Calculos():
+    try:
+        Entrada_1 = int(input('Digite o primeiro numero: '))
+        Entrada_2 = int(input('Digite o segundo numero: '))
 
-except ZeroDivisionError:
-    print('Não e possivel dividir por ZERO!')
-except ValueError:
-    print('Entradas Invalidas')
-except KeyboardInterrupt:
-    print('Usuario encerrou o processo')
-except:
-    print('Erro Desconhecido')
+        Resultado = Calcular(Entrada_1,Entrada_2)
+
+        print('Estamos fazendo calculo, aguarde...')
+        sleep(2)
+        print('So mais um momento...')
+        sleep(2)
+
+        print(f'''
+        Resultado:
+        A Soma é de: {Resultado.somar()}
+        A Subtração é de: {Resultado.Subtrair()}
+        A Multiplicação é de: {Resultado.multiplicar()}
+        A Divisão é de: {Resultado.Dividir():.2f}
+        ''')
+        App()
+    
+    except ZeroDivisionError:
+        print('Não e possivel dividir por ZERO!')
+    except ValueError:
+        print('Entradas Invalidas')
+    except KeyboardInterrupt:
+        print('Usuario encerrou o processo')
+    except:
+        print('Erro Desconhecido')
+
+
+Calculos()
